303707171ee3c75ed77d038f486bf5d234d63f8a
GustavoEmiliano <gustavoemiliano2014@gmail.com>
2026-03-22 18:12:16 -0300

    feat(ui): add Bootstrap styling and create navbar component
    - integrated Bootstrap into the application styles
    
    - created navbar component for layout structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gerador-certificado';
+  exibeNavbar: boolean = true;
 }
